8e48147d07c3da2e87cf4bf186dc104e462bcac1
dennyzain <abbasdenny24@gmail.com>
2022-05-10 08:49:39 +0700

    fix text resource

--- components/molecules/PovResourceSection.js
+++ components/molecules/PovResourceSection.js
@@ -4,7 +4,7 @@ export default function PovResourceSection() {
       <div className="px-[32px] pt-[162px] bg-black h-auto xl:px-[373.5px] ">
         <h3 className="font-black text-[32px] text-white xl:text-center">POV</h3>
         <p className="text-white leading-[24px] font-normal mt-[30px] text-base xl:text-[18px] xl:leading-[27px] xl:text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris nisi ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-        <h3 className="font-black text-[32px] pt-[67px] text-white xl:text-center">RESOURCE</h3>
+        <h3 className="font-black text-[32px] pt-[67px] text-white xl:text-center">Resource</h3>
         <p className="text-white leading-[24px] font-normal mt-[30px] text-base xl:text-[18px] xl:leading-[27px] xl:text-center">These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best</p>
       </div>
     </section>
